100398ea036652e698dbdd53e1dbc042afc2039f
TobiNguyen <nat21102002@gmail.com>
2023-08-26 03:30:53 +0700

    lession2: middleware

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import Middleware from './app/configs/middleware';
 
 class App {
     public app: express.Application;
 
     constructor() {
         this.app = express();
+        Middleware.init(this);
     }
 }
 
