42694923b4a87e7bdd029b15a8ef06ee2ed452fb
TobiNguyen <nat21102002@gmail.com>
2023-09-01 23:29:14 +0700

    task7: swagger init

--- app/router/index.ts
+++ app/router/index.ts
@@ -1,12 +1,15 @@
-import * as express from 'express';
 import { IServer } from '../interfaces/serverInterface';
 import CategoryRouter from './category';
 import NewsRouter from './news';
 import PriceRouter from './price';
 import ArticleRouter from './article';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../lib/swagger.json';
 
 export default class IndexRouter {
     static init(server: IServer) {
+        // doc api
+        server.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
         // Article
         server.app.use('/api/v1/article', new ArticleRouter().router);
