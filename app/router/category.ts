1de63a8d6b5b83813eea7f67065d5c0a7d6f9048
TobiNguyen <nat21102002@gmail.com>
2023-09-04 14:01:36 +0700

    swagger c1

--- app/router/category.ts
+++ app/router/category.ts
@@ -15,8 +15,8 @@ export default class ItemRouter {
         this.router.get('/', asyncHandle(item_Controller.get));
         this.router.get('/:id', asyncHandle(item_Controller.getOne));
         this.router.get('/:id/article(/:total)?', asyncHandle(item_Controller.getArticleInCategory));
-        this.router.post('/add', validator, asyncHandle(item_Controller.post));
-        this.router.put('/edit/:id', validator, asyncHandle(item_Controller.put));
-        this.router.delete('/delete/:id', asyncHandle(item_Controller.delete));
+        this.router.post('/', validator, asyncHandle(item_Controller.post));
+        this.router.put('/:id', validator, asyncHandle(item_Controller.put));
+        this.router.delete('/:id', asyncHandle(item_Controller.delete));
     }
 }
